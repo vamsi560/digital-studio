39f156bd35967c4760dff79754392bb44c7c58e8
Sapireddy Vamsi <sapireddyvamsi@gmail.com>
2025-07-08 16:28:15 +0000

    Can you update the api key with this api key and hardcode it for now we

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: process.env.GOOGLE_API_KEY})],
+  plugins: [googleAI({apiKey: 'AIzaSyAlW859ro344ulhwmTJKwWmYx8uHiTa2IE'})],
   model: 'googleai/gemini-1.5-pro-latest',
 });
