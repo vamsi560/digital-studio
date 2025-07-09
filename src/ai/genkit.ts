04e94c134f0db8d58585a7e12ef65fe67329a798
Sapireddy Vamsi <sapireddyvamsi@gmail.com>
2025-07-09 10:24:41 +0000

    # # Fatal process out of memory: Zone # ----- Native stack trace -----

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,5 +3,5 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI({apiKey: 'AIzaSyAlW859ro344ulhwmTJKwWmYx8uHiTa2IE'})],
-  model: 'googleai/gemini-1.5-pro-latest',
+  model: 'googleai/gemini-1.5-flash-latest',
 });
